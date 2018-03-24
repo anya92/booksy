46c31f63a5fdc3f9b18381fd15be91ac14e30535
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2018-03-24 17:40:15 +0100

    created filter for myShelf component

--- src/client/pages/MyShelf.js
+++ src/client/pages/MyShelf.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import { graphql } from 'react-apollo';
 
@@ -16,22 +16,133 @@ const Filter = styled.div`
   border-bottom: 1px solid #eee;
   padding: 20px;
   margin: 20px 0;
+  display: flex;
+  justify-content: space-between;
 `;
 
-const MyShelf = ({ data, auth }) => {
-  if (data.loading) return <div>Loading...</div>;
-  if (data.error) return <div>Error</div>;
-  
-  return (
-    <div>
-      <Title>My shelf</Title>
-      <Filter>
-        <span>All</span>
-        <span>By date</span>
-      </Filter>
-      <BooksGrid books={data.userBooks} auth={auth} />
-    </div>
-  );
+const DropdownContent = styled.div`
+  display: none;
+  position: absolute;
+  left: 0;
+  top: 30px;
+  background-color: #f9f9f9;
+  min-width: 140px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+`;
+
+const Dropdown = styled.div`
+  position: relative;
+  &.down {
+    ${DropdownContent} {
+      display: block;
+    }
+  }
+`;
+
+const DropdownButton = styled.button`
+  font-weight: 700;
+  cursor: pointer;
+  background: transparent;
+  border: none;
+  color: #333;
+  &:active, &:focus {
+    outline: none;
+  }
+  i {
+    margin-left: 5px;
+  }
+`;
+
+
+
+const DropdownLink = styled.div`
+  cursor: pointer;
+  padding: 12px 16px;
+  &:hover {
+    background: rgba(0,0,0,0.2);
+  }
+`;
+
+class MyShelf extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      category: 'all',
+      filteredBooks: [],
+    };
+  }
+
+  componentWillReceiveProps({ data: { userBooks } }) {
+    this.setState(() => ({
+      category: 'all',
+      filteredBooks: userBooks, 
+    }));
+  }
+
+  setFilter(category) {
+    const { data: { userBooks } } = this.props;
+    const filteredBooks = userBooks.filter(book => (
+      category == 'all' ? book : book.category == category
+    ));
+
+    this.setState(() => ({
+      category,
+      filteredBooks
+    }), () => this.toggleDropdown());
+  }
+
+  toggleDropdown() {
+    this.categoryDropdown.classList.toggle('down');
+
+    if (this.categoryDropdown.classList.contains('down')) {
+      this.categoryChevron.classList.remove('fa-chevron-down');
+      this.categoryChevron.classList.add('fa-chevron-up');
+    } else {
+      this.categoryChevron.classList.remove('fa-chevron-up');
+      this.categoryChevron.classList.add('fa-chevron-down');
+    }
+  }
+
+  render() {
+    const { data, auth } = this.props;
+
+    if (data.loading) return <div>Loading...</div>; // todo -> loading component
+    if (data.error) return <div>Error</div>; // todo -> error component
+    
+    let categories = new Set();
+    data.userBooks.forEach(book => categories.add(book.category));
+
+    return (
+      <div>
+        <Title>My shelf</Title>
+        <Filter>
+          <Dropdown innerRef={ref => (this.categoryDropdown = ref)}>
+            <DropdownButton onClick={this.toggleDropdown.bind(this)}>
+              {this.state.category} 
+              <i ref={ref => (this.categoryChevron = ref)} className="fa fa-chevron-down" />
+            </DropdownButton>
+            <DropdownContent>
+              <DropdownLink onClick={() => this.setFilter('all')}>all</DropdownLink>
+              {
+                [...categories].map((category, i) => (
+                  <DropdownLink key={i} onClick={() => this.setFilter(category)}>
+                    {category.toLowerCase()}
+                  </DropdownLink>
+                ))
+              }
+            </DropdownContent>
+          </Dropdown>
+          <div>Sort by: date</div>
+        </Filter>
+        <BooksGrid 
+          books={this.state.filteredBooks} 
+          auth={auth}
+        />
+      </div>
+    );
+  }
 };
 
 export default graphql(FETCH_USER_BOOKS_QUERY)(MyShelf);
