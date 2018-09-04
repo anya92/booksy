fccc8a1eba5b465ff7c99fd1b4641ca5b5683f04
Ania <aniabrajtkopf@gmail.com>
2018-09-04 11:06:59 +0200

    changed filter and sort buttons

--- src/client/components/FilterAndSort.js
+++ src/client/components/FilterAndSort.js
@@ -1,128 +1,67 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import SortIcon from "@material-ui/icons/Sort";
+import FilterListIcon from "@material-ui/icons/FilterList";
 
-const Filter = styled.div`
-  border-top: 1px solid #eee;
-  border-bottom: 1px solid #eee;
-  padding: 10px 0;
-  margin: 20px 0;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  grid-gap: 20px;
-`;
+const sortOptions = [
+  'date added (newest)',
+  'date added (oldest)',
+  'title (A-Z)',
+  'title (Z-A)',
+];
 
-const DropdownContent = styled.div`
-  display: none;
-  position: absolute;
-  top: 30px;
-  left: 0;
-  background-color: #f9f9f9;
-  min-width: 140px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-`;
-
-const Dropdown = styled.div`
-  position: relative;
-  &.down {
-    ${DropdownContent} {
-      display: block;
-    }
-  }
-`;
-
-const DropdownButton = styled.button`
-  font-weight: 700;
-  cursor: pointer;
-  background: transparent;
-  border: none;
-  color: #333;
-  &:active, &:focus {
-    outline: none;
-  }
-  i {
-    margin-left: 5px;
-  }
-`;
-
-const DropdownLink = styled.div`
-  cursor: pointer;
-  padding: 12px 16px;
-  &:hover {
-    background: rgba(0,0,0,0.2);
-  }
-`;
+const styles = theme => ({
+  container: {
+    borderBottom: '1px solid #fff',
+    margin: '10px 0',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(2, 1fr)',
+    gridGap: '20px',
+  },
+  icon: {
+    fontSize: '20px',
+    marginRight: theme.spacing.unit,
+  },
+});
 
 class FilterAndSort extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      category: 'all',
-      sortBy: 'date added',
-      descending: true,
-    };
+  state = {
+    sortAnchorEl: null,
+    categoryAnchorEl: null,
+    category: 'all',
+    sortBy: 'date added (newest)',
   }
 
-  setFilter(category) {
-    this.setState(() => ({ category }), () => {
-      this.toggleFilter();
-      this.filterAndSort();
-    });
-  }
-
-  filterBooks(books) {
+  filterBooks = books => {
     const { category } = this.state;
     const filteredBooks = books.filter(book => (
-      category == 'all' ? book : book.category == category
+      category == 'all' ? book : book.category.toLowerCase() == category
     ));
 
     return filteredBooks;
   }
 
-  setSortBy(sortBy) {
-    this.setState(prevState => ({
-      descending: sortBy == prevState.sortBy ? !prevState.descending : true,
-      sortBy,
-    }), () => {
-      this.toggleSort();
-      this.filterAndSort();
-    });
-  }  
-
-  sortBooks(books) {
-    const { descending, sortBy } = this.state;
-    // sort books
-    let sortedBooks;
-
-    if (sortBy == 'date added') {
-      sortedBooks = descending
-      ? [...books].sort((a, b) => new Date(b.added) - new Date(a.added))
-      : [...books].sort((a, b) => new Date(a.added) - new Date(b.added));
-    } else {
-      sortedBooks = descending
-      ? [...books].sort((a, b) => a.title > b.title)
-      : [...books].sort((a, b) => a.title < b.title);
-    }
-    return sortedBooks;
-  }
-
-  toggleFilter() {
-    this.filterDropdown.classList.toggle('down');
-    if (this.filterDropdown.classList.contains('down')) {
-      this.categoryChevron.classList.remove('fa-chevron-down');
-      this.categoryChevron.classList.add('fa-chevron-up');
-    } else {
-      this.categoryChevron.classList.remove('fa-chevron-up');
-      this.categoryChevron.classList.add('fa-chevron-down');
+  sortBooks = books => {
+    const { sortBy } = this.state;
+ 
+    switch (sortBy) {
+      case 'date added (newest)':
+        return [...books].sort((a, b) => new Date(b.added) - new Date(a.added));
+      case 'date added (oldest)':
+        return [...books].sort((a, b) => new Date(a.added) - new Date(b.added));
+      case 'title (A-Z)':
+        return [...books].sort((a, b) => a.title > b.title);
+      case 'title (Z-A)':
+        return [...books].sort((a, b) => a.title < b.title);
+      default:
+        return books;
     }
   }
 
-  toggleSort() {
-    this.sortDropdown.classList.toggle('down');
-  }
-
-  filterAndSort() {
+  filterAndSort = () => {
     const { books, setBooks } = this.props;
     // filter books
     const filteredBooks = this.filterBooks(books);
@@ -132,56 +71,103 @@ class FilterAndSort extends Component {
     setBooks(sortedBooks);
   }
 
-  render() {
-    let categories = new Set();
-    this.props.books.forEach(book => (book.category && categories.add(book.category)));
-    const { descending, sortBy } = this.state;
+  handleSortOpen = e => {
+    this.setState({ sortAnchorEl: e.currentTarget });
+  }
 
+  handleCategoryOpen = e => {
+    this.setState({ categoryAnchorEl: e.currentTarget });
+  }
+
+  handleSortClose = () => {
+    this.setState({ sortAnchorEl: null });
+  }
+
+  handleCategoryClose = () => {
+    this.setState({ categoryAnchorEl: null });
+  }
+
+  handleSortItemClick = option => {
+    this.setState(() => ({
+      sortAnchorEl: null,
+      sortBy: option,
+    }), () => this.filterAndSort());
+  }
+
+  handleCategoryItemClick = category => {
+    this.setState(() => ({
+      categoryAnchorEl: null,
+      category,
+    }), () => this.filterAndSort());
+  }
+
+  render() {
+    let categories = new Set(['all']);
+    this.props.books.forEach(book => (book.category && categories.add(book.category.toLowerCase())));
+
+    const { classes } = this.props;
     return (
-      <Filter>
-        <Dropdown innerRef={ref => (this.filterDropdown = ref)}>
-          Category:
-          <DropdownButton onClick={this.toggleFilter.bind(this)}>
-            { this.state.category.toLowerCase() } 
-            <i ref={ref => (this.categoryChevron = ref)} className="fa fa-chevron-down" />
-          </DropdownButton>
-          <DropdownContent>
-            <DropdownLink onClick={() => this.setFilter('all')}>all</DropdownLink>
+      <div className={classes.container}>
+        <div>
+          <Button
+            aria-haspopup="true"
+            aria-controls="category-menu"
+            aria-label="Category"
+            onClick={this.handleCategoryOpen}
+          >
+            <FilterListIcon className={classes.icon} /> Category
+          </Button>
+          <Menu
+            id="category-menu"
+            anchorEl={this.state.categoryAnchorEl}
+            open={Boolean(this.state.categoryAnchorEl)}
+            onClose={this.handleCategoryClose}
+          >
             {
-              [...categories].map((category, i) => (
-                <DropdownLink key={i} onClick={() => this.setFilter(category)}>
-                  { category.toLowerCase() }
-                </DropdownLink>
+              [...categories].map(category => (
+                <MenuItem
+                  key={category}
+                  selected={category === this.state.category}
+                  onClick={() => this.handleCategoryItemClick(category)}
+                >
+                  {category}
+                </MenuItem>
               ))
             }
-          </DropdownContent>
-        </Dropdown>
-        <Dropdown innerRef={ref => (this.sortDropdown = ref)}>
-          Sort by:
-          <DropdownButton onClick={this.toggleSort.bind(this)}>
-            {this.state.sortBy} 
+          </Menu>
+        </div>
+        <div>
+          <Button
+            
+            aria-haspopup="true"
+            aria-controls="sort-menu"
+            aria-label="Sort by"
+            onClick={this.handleSortOpen}
+          >
+            <SortIcon className={classes.icon} /> Sort by
+          </Button>
+          <Menu
+            id="sort-menu"
+            anchorEl={this.state.sortAnchorEl}
+            open={Boolean(this.state.sortAnchorEl)}
+            onClose={this.handleSortClose}
+          >
             {
-              this.state.descending 
-              ? <i className="fa fa-angle-double-down" />
-              : <i className="fa fa-angle-double-up" />
+              sortOptions.map(option => (
+                <MenuItem
+                  key={option}
+                  selected={option === this.state.sortBy}
+                  onClick={() => this.handleSortItemClick(option)}
+                >
+                  { option }
+                </MenuItem>
+              ))
             }
-          </DropdownButton>
-          <DropdownContent>
-            <DropdownLink onClick={() => this.setSortBy('date added')}>
-              date added { sortBy == 'date added' && (descending 
-              ? <i className="fa fa-angle-double-up" /> 
-              : <i className="fa fa-angle-double-down" />) }
-            </DropdownLink>
-            <DropdownLink onClick={() => this.setSortBy('title')}>
-              title { sortBy == 'title' && (descending 
-              ? <i className="fa fa-angle-double-up" /> 
-              : <i className="fa fa-angle-double-down" />) }
-            </DropdownLink>
-          </DropdownContent>
-        </Dropdown>
-      </Filter>
+          </Menu>
+        </div>
+      </div>
     );
   }
 }
 
-export default FilterAndSort;
+export default withStyles(styles)(FilterAndSort);
